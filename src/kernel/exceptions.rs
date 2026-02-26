edf42990178a6f7e128e5ecf014756c9050c2644
Aayan <zaacky2456@gmail.com>
2026-02-26 23:38:44 +0530

    [exceptions] ctx pretty printing

--- src/kernel/exceptions.rs
+++ src/kernel/exceptions.rs
@@ -24,5 +24,25 @@ pub struct ExceptionContext {
 // called by `exceptions.s`
 #[no_mangle]
 pub extern "C" fn handle_exception_el1(ctx: &mut ExceptionContext) {
-    println!("An exception has been detected >xD {}", ctx.etype as u8).unwrap();
+    
+    println!("An exception has been detected :D").unwrap();
+    
+    // printing the full context for now.
+    let etype_str = match ctx.etype {
+        ExceptionType::_SYNC => "SYNC",
+        ExceptionType::_IRQ  => "IRQ",
+        ExceptionType::_FIQ  => "FIQ",
+        ExceptionType::_SE   => "SError",
+    };
+    println!("=== Exception Context ===").unwrap();
+    println!("Type : {} ({})", etype_str, ctx.etype as u8).unwrap();
+    println!("ELR  : {:#018x}", ctx.elr).unwrap();
+    println!("SPSR : {:#018x}", ctx.spsr).unwrap();
+    println!("ESR  : {:#018x}", ctx.esr).unwrap();
+    println!("FAR  : {:#018x}", ctx.far).unwrap();
+    println!("Registers:").unwrap();
+    for i in 0..31 {
+        println!("  x{:02} = {:#018x}", i, ctx.x[i]).unwrap();
+    }
+    println!("=========================").unwrap();
 }
